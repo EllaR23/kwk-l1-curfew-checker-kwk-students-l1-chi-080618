a0dfa5f7ee662c22bd05adef62747af049d01c9f
EllaR23 <ellaratarac@gmail.com>
2018-08-07 19:39:16 +0000

    automatically backed up by learn

--- curfew_checker.rb
+++ curfew_checker.rb
@@ -9,12 +9,12 @@
 # end
 # simple_curfew_checker(time)
 
-# def martha(number)
-#   if number == 3
-#     puts "the number is 3"
-#   elsif number == 5
-#     puts "the number is 5"
-#   else
-# end
-# end
-# # martha(3)
+def martha(number)
+  if number == 3
+    puts "the number is 3"
+  elsif number == 5
+    puts "the number is 5"
+  else
+end
+end
+# martha(3)
